14ecc265126ce6da74d332ca7382dbfeed33ce08
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2020-05-04 17:15:29 -0400

    programmatically list classes

--- __init__.py
+++ __init__.py
@@ -4,34 +4,21 @@ mustafa@cs.toronto.edu
 
 Created by Mustafa Quraish
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+---
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
-from .operators.Scratch import Scratch
-from .operators.line_between_points import LineBetweenPoints
-from .operators.draw_triangle import DrawTriangle
-from .operators.draw_circumcircle import DrawCircumcircle
-from .operators.draw_inscribed_circle import DrawInscribedCircle
-from .operators.bisect_plane import BisectPlane
-from .operators.empty_orthocenter import EmptyOrthocenter
-from .operators.empty_circumcenter import EmptyCircumcenter
-from .operators.empty_barycenter import EmptyBarycenter
-from .operators.empty_incenter import EmptyIncenter
-from .operators.empty_middle import EmptyMiddle
-from .operators.points_plane import PointsPlane
-import bpy
-
 
 bl_info = {
     "name": "GeoBlender",
@@ -39,29 +26,29 @@ bl_info = {
     "author": "Mustafa Quraish",
     "version": (1, 0, 0),
     "blender": (2, 80, 0),
-    "wiki_url": "https://github.com/mustafaquraish",
+    "wiki_url": "https://github.com/mustafaquraish/GeoBlender",
     "tracker_url": "https://github.com/mustafaquraish",
     "category": "Geometry"
 }
 
-classes = (
-    LineBetweenPoints,
-    PointsPlane,
-    BisectPlane,
-    DrawTriangle,
-    EmptyMiddle,
-    EmptyCircumcenter,
-    EmptyBarycenter,
-    EmptyIncenter,
-    EmptyOrthocenter,
-    DrawCircumcircle,
-    DrawInscribedCircle,
-)
+import bpy
+from sys        import modules 
+from inspect    import getmembers, isclass
 
+# Import operator classes
+from .operators.Scratch                 import Scratch
+from .operators.bisect_plane            import BisectPlane
+from .operators.empty_middle            import EmptyMiddle
+from .operators.points_plane            import PointsPlane
+from .operators.draw_triangle           import DrawTriangle
+from .operators.empty_incenter          import EmptyIncenter
+from .operators.empty_barycenter        import EmptyBarycenter
+from .operators.empty_orthocenter       import EmptyOrthocenter
+from .operators.draw_circumcircle       import DrawCircumcircle
+from .operators.empty_circumcenter      import EmptyCircumcenter
+from .operators.line_between_points     import LineBetweenPoints
+from .operators.draw_inscribed_circle   import DrawInscribedCircle
+
+# Register all classes in the current module
+classes = (obj for _, obj in getmembers(modules[__name__]) if isclass(obj))
 register, unregister = bpy.utils.register_classes_factory(classes)
-
-# def register():
-#     bpy.utils.register_module(__name__)
-
-# def unregister():
-#     bpy.utils.unregister_module(__name__)
