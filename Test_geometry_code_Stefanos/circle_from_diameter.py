c9dd44530c4bdbb474cb77c98b4490363ccc6c8e
stefanosaretakis <65354072+stefanosaretakis@users.noreply.github.com>
2020-05-25 20:22:34 -0400

    Update circle_from_diameter.py

--- Test_geometry_code_Stefanos/circle_from_diameter.py
+++ Test_geometry_code_Stefanos/circle_from_diameter.py
@@ -1,4 +1,5 @@
 def circle_from_diameter(obj, A, B, hide_extra=True):
+    
     '''
     Finds the circle with diameter AB. It first puts an empty O at the midpoint
     of AB and     then draws the circle with center O and radius equal to half AB. 
@@ -12,8 +13,6 @@ def circle_from_diameter(obj, A, B, hide_extra=True):
     # Place the circle obj with center at the midpoint
 
     put_in_between(obj, A, B, 0.5)
-    
-    # Add_driver(obj, scale, 'XYZ', vars_def={'d': ('distance', A, B)}, expr="0.5*d"):
     
     add_driver_distance(obj, scale, 'XYZ', A, B, 0.5)
 
